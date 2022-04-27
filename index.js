6a4f30ccffade8f176ff119f7f4e10b3140afdca
Hubert Lemczak <hubertlemczak@gmail.com>
2022-04-27 19:21:42 +0100

    commit

---
+++ index.js
@@ -0,0 +1,43 @@
+// let firstName = 'Hubert';
+// let lastName = 'Lemczak';
+
+// let fullName = firstName + ' ' + lastName;
+// console.log(fullName);
+// console.log('Your name has ' + (fullName.length - 1) + ' characters');
+
+// console.log(`Your name has ${fullName.length - 1} characters`);
+
+// let names = 'shaun';
+
+// console.log(names > 'zhaun');
+
+// for (i = 0; i < 5; i++) {
+//   console.log(i);
+// }
+
+// let password = 'password';
+
+// if (password.length >= 10) {
+//   console.log('nice');
+// } else {
+//   console.log('need stronger password');
+// }
+
+// // || OR && AND
+
+// // arrow function
+// const greet = () => 'hello, world';
+// let result = greet();
+// console.log(result);
+
+// let addExclamation = () => '!';
+// const result = addExclamation();
+// console.log('How did my test go?' + result);
+
+let people = ['mario', 'luigi', 'ryu', 'shaun', 'chun-li'];
+
+const logPerson = (person, index) => {
+  console.log(`${index} - hello ${person}.`);
+};
+
+people.forEach(logPerson);
